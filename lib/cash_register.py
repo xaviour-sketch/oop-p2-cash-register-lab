64e7bba6f62c2ac2b8daf99cf42e1eaf4f07ef9a
xaviour-sketch <xaviourmuthee24@gmail.com>
2026-06-10 08:46:42 +0300

    new commit

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -1,4 +1,47 @@
 #!/usr/bin/env python3
 
 class CashRegister:
-  pass
+    def __init__(self, discount=0):
+        self.discount = discount
+        self.total = 0
+        self.items = []
+        self.previous_transactions = []
+
+    @property
+    def discount(self):
+        return self._discount
+
+    @discount.setter
+    def discount(self, value):
+        if not isinstance(value, int) or not (0 <= value <= 100):
+            print("Not valid discount")
+            self._discount = 0
+        else:
+            self._discount = value
+
+    def add_item(self, item, price, quantity=1):
+        self.total += price * quantity
+        self.items.append(item)
+        self.previous_transactions.append({
+            "item": item,
+            "price": price,
+            "quantity": quantity
+        })
+
+    def apply_discount(self):
+        if not self.previous_transactions:
+            print("There is no discount to apply.")
+            return
+
+        last = self.previous_transactions.pop()
+        discounted_price = last["price"] * last["quantity"] * (self.discount / 100)
+        self.total -= discounted_price
+        self.items.remove(last["item"])
+
+    def void_last_transaction(self):
+        if not self.previous_transactions:
+            return
+
+        last = self.previous_transactions.pop()
+        self.total -= last["price"] * last["quantity"]
+        self.items.remove(last["item"])
